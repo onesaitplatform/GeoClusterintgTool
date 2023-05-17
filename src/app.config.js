405b130dbff27ae6e2962f810f3e27e87bd63b00
Fco. Javier López Acevedo <fjlacevedo@minsait.com>
2023-05-17 17:16:42 +0200

    Env added

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+export const appConfigUrl = process.env.VUE_APP_FRONT_CONFIG_URL
+export const mapConfigUrl = process.env.VUE_APP_MAP_CONFIG_URL
+export const geoserverUrl = process.env.VUE_APP_GEOSERVER_URL
+export const baseUrl = process.env.VUE_APP_API_BASE_URL
+
+// export default {
+// 	appConfigUrl,
+// 	mapConfigUrl,
+//   baseUrl
+// }
